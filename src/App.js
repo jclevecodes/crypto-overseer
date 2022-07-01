08d4b2b776fa2e733fc7abae53949b2ffca1f43b
jclevecodes <joshua.cleveland02@gmail.com>
2022-07-01 12:55:16 -0400

    axios setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
+
+    const [coins, setCoins] = useState([])
+
+    useEffect(() => {
+      axios.get(url).then((response) => {
+        setCoins(response.data)
+        console.log(response.data[0])
+      }).catch((error) => {
+        console.log(error)
+      })
+    }, [])
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
